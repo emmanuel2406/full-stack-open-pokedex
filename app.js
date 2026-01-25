572c16b409fd542f9146ad34c40caa77a54b0eb3
emmanuel2406 <emmanuelmichaelrassou@gmail.com>
2026-01-25 17:29:41 -0500

    Fixing forced error

--- app.js
+++ app.js
@@ -12,11 +12,9 @@ app.listen(PORT, () => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('9')
+  res.send('10')
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw new Error('Health check failed')
   res.send('ok')
 })
